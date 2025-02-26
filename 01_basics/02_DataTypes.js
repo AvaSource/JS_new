4e09735cb6bfeb53e4e8e33140d033939d448c74
AvaSource <avanco140@gmail.com>
2025-02-26 16:41:46 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treats all JS code as a newer version 
+
+//alert (3 + 3) // This can be used in browser not in node JS
+
+let name = "Kaavya"
+let age = 20
+let isLoggedIn = false
+let state
+
+//primitive DataTypes
+// number => 2 to the power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
