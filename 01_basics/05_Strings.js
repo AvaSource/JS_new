8fa6200b01397acdc122ff81c92603bee47f2756
AvaSource <avanco140@gmail.com>
2025-03-02 17:17:56 +0000

    strings in JS

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,35 @@
+const name = "Kaavya"
+const repoCount = 2
+
+//console.log(name + repoCount + "Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// use this kind of syntax
+
+const gameName = new String ("kaavya-llc-com")
+
+// console.log(gameName[4]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(8));
+//console.log(gameName.indexOf('v'));
+
+const newString = gameName.substring(0, 4) 
+//console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+//console.log(anotherString);
+
+const newString1 = "      Kaavya     "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const url = "https://kaavya.com/kaavya%20desai"
+// console.log(url.replace('%20', '-'));
+ 
+// console.log(url.includes('kaavya'));
+// console.log(url.includes('kavni'));
+ 
+console.log(gameName.split('-'));
